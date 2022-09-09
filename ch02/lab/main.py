baba3e9c664b80d98379e4e2d7c471fed1386bc1
Michelle Lu <112450739+michellelu2@users.noreply.github.com>
2022-09-09 21:02:40 +0000

    Lab 2

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,10 +1,21 @@
-
+import random
 #Part A
 weeks = 16
 classes = 5
 tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
-
+classes_per_week = 3
+cost_per_class = cost_per_week / classes_per_week
+print ("Your cost per class each week is: " + str(cost_per_class))
+print (weeks, type(weeks))
+print (classes, type(classes))
+print (tuition, type(tuition))
+print (cost_per_week, type(cost_per_week))
+print (classes_per_week, type(classes_per_week))
+print (cost_per_class, type(cost_per_class))
 
 #Part B
+fruits = ("apple", "bananna", "tangerine", "watermelon", "strawberry")
+random = random.choice(fruits)
+print ("The fruit that has been selected is: " + (random))
