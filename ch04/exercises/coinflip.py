9ee4a682d4b9c91eae3edc82d1842fc4f150667b
Michelle Lu <112450739+michellelu2@users.noreply.github.com>
2022-10-10 21:11:20 +0000

    Coin Flip

--- ch04/exercises/coinflip.py
+++ ch04/exercises/coinflip.py
@@ -3,21 +3,37 @@ import random
 
 window = turtle.Screen()
 myturtle = turtle.Turtle()
-myturtle.color('blue')
 myturtle.shape('turtle')
-myturtle.goto(0,0)
 
-coinflip = ['heads' , 'tails']
+colors = ['red' , 'blue' , 'green']
 
-random = random.choice(coinflip)
+myturtle.speed(0)
 
-for i in random:
-  print (i)
+distance = 10
+angle = 90
 
+in_screen = True
 
+while in_screen:
+  coin = random.randrange(0,2)
+  if coin == 0:
+    myturtle.right(angle)
+  else:
+    myturtle.left(angle)
+  myturtle.forward(distance)
 
+  turtleX =  myturtle.xcor()
+  turtleY =  myturtle.ycor()
 
+  x_range = window.window_width()/2
+  y_range = window.window_height()/2
+
+  myturtle.color(colors[0])
+
+  colors.append(colors.pop(0))
+
+  if abs(turtleX) > x_range or abs(turtleY) > y_range:
+    in_screen = False
+
+window.bgcolor('pink')
 window.exitonclick()
-
-
-
