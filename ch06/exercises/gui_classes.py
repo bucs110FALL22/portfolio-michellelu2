9f5d9cd0ca105129344de9d844371320a056a5ea
Michelle Lu <112450739+michellelu2@users.noreply.github.com>
2022-10-24 22:02:09 +0000

    GUI Classes

---
+++ ch06/exercises/gui_classes.py
@@ -0,0 +1,20 @@
+class Cloud:
+  def __init__(self):
+    self.color = 'white'
+    self.heigh = 5
+    self.forward = 200
+
+class Mushroom1:
+  def __init__(self):
+    self.color = 'red'
+    self.lives = 1
+    self.large = False
+
+
+class Block1:
+  def __init__(self):
+    self.color = 'red'
+    self.width = 2
+    self.height = 2
+    
+    
